7bb946447a30a3aad3276509a7c5c906e84ec0e6
Yoshio Nishimoto <nishimoto@kuchem.kyoto-u.ac.jp>
2026-08-14 11:13:38 +0900

    Initialize HONE in MkFock (just in case), and update some comments

--- src/caspt2/mkfock.F90
+++ src/caspt2/mkfock.F90
@@ -13,6 +13,7 @@
 subroutine MkFock(CMO,nCMO,FIMO,NFIMO,FIFA,nFIFA,DREF,nDREF,HONE,nHONE,INITIATE)
 
 use caspt2_module, only: IfChol
+use Constants, only: Zero
 use Definitions, only: wp, iwp
 
 implicit none
@@ -27,9 +28,13 @@ logical(kind=iwp), intent(inout) :: INITIATE
 if (IfChol) then
   ! INTCTL2 uses TraCho2 to generate the fock matrix in AO basis. Subsequently, FMatCho
   ! transform to the MO basis.
-  ! TraOne, which calculates HONE = one-electron Hamiltonian + G(D^frozen), is not necessary.
-  ! The one-electron Hamiltonian and G(D^{inactive+frozen}) are computed and added in TraCho2 (to FFIAO).
+  ! The one-electron Hamiltonian and G(D^{inactive+frozen}) are computed and added in TraCho2 (to FFIAO),
+  ! so FIMO (and FIFA) remains unchanged.
   call INTCTL2(CMO,NCMO,DREF,nDREF,FIFA,nFIFA,FIMO,nFIMO)
+  ! TraOne, which calculates HONE = one-electron Hamiltonian + G(D^frozen), is not necessary,
+  ! because HONE is not used anywhere at present; if HONE is used in the future, this must be constructed.
+  ! However, there should be little point in separating h + G(D^frozen) from G(D^inactive)
+  if (Initiate) HONE(1:nHONE) = Zero
 else
   ! Matrix elements generated directly from one-ham and two-electron integrals in th MO basis.
   if (Initiate) call TraOne(CMO,nCMO,HONE,nHONE)
